2191c4142905670c7769c308f560bf33eb235375
Luis Mejia <luismejia69@gmail.com>
2022-08-24 18:47:54 -0500

    LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -24,23 +24,28 @@ class LIFOCache(BaseCaching):
         """
 
         def IsCacheFull() -> bool:
+            """ Verify is cached it's full filled"""
             if len(self.cache_data) == self.MAX_ITEMS:
                 return True
             return False
 
-        def IsNewKey(key):
+        def IsNewKey(key) -> bool:
+            """ Verify if the key to add to cache is new or already exists"""
             keys = [x for x in self.cache_data]
             if key in keys:
                 return False
             return True
 
         def PopLast():
+            """deletes the last item in to cache"""
             self.cache_data.pop(self.__last_update)
 
         def AddToCache(key, item):
+            """add or update a key in cache"""
             self.cache_data.update({key: item})
 
         def UpdateLast(key):
+            """update the control var last_update"""
             self.__last_update = key
 
         isFull: bool = IsCacheFull()
