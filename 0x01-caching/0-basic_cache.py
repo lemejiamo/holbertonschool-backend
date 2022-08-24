03018067de17bebe0f2dffe1da4e163423ddfe1a
Luis Mejia <luismejia69@gmail.com>
2022-08-24 16:29:12 -0500

    simple cache dict

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -39,16 +39,22 @@ class BasicCache(BaseCaching):
     """
 
     def __init__(self):
-        """ CONSTRUCTOR """
+        """ 
+        CONSTRUCTOR 
+        """
         super().__init__()
 
     def put(self, key, item):
-        """ update the key in the cache data """
+        """
+        update the key in the cache data
+        """
         if key is not None:
             self.cache_data.update({key: item})
         else:
             pass
 
     def get(self, key):
-        """ Return the valur from the given key if exists """
+        """
+        Return the valur from the given key if exists
+        """
         return (self.cache_data.get(key))
